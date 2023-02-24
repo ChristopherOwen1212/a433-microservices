45ae948bab54323309da237cca09bc9848e6a499
Christopher Owen <christo1212@gmail.com>
2023-02-24 17:35:43 +0700

    Add script to build and push image to Docker Hub

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Proyek Deploy Aplikasi Karsa Jobs dengan Kubernetes
+
+# Membuat Docker image dari Dockerfile yang disediakan
+docker build -t carbonite12/karsajobs-ui:latest .
+
+# Login ke Docker Hub via Terminal
+echo $PASSWORD_DOCKER_HUB | docker login -u carbonite12 --password-stdin
+
+# Mengunggah image ke Docker Hub
+docker push carbonite12/karsajobs-ui:latest
